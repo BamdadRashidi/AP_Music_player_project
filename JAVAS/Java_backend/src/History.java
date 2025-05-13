196631b5b9a8dffc50f0722ad8b0b253dac25d5d
Mohammadamin Rashidi <bamdadrshydy@gmail.com>
2025-05-12 23:00:28 -0700

    polished History class

---
+++ JAVAS/Java_backend/src/History.java
@@ -0,0 +1,37 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class History {
+    private static final int Max = 10;
+    private List<Track> history;
+
+    public History() {
+        history = new LinkedList<>();
+    }
+
+
+    public void addToHistory(Track song) {
+        if (!history.contains(song)) {
+            if (history.size() >= Max) {
+                history.remove(0);
+            }
+            history.add(song);
+        }
+    }
+
+    public List<Track> getLastPlayedSongs() {
+        return history;
+    }
+
+
+    public void displayHistory() {
+        if (history.isEmpty()) {
+            System.out.println("There is no song");
+        } else {
+            for (Track song :history) {
+                System.out.println( song.getArtistName());
+            }
+        }
+    }
+}
+
