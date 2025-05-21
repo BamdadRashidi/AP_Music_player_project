f350947de71275409b5c9c479d54b311a8e13152
imanth11 <imanhunter44@gmail.com>
2025-05-21 10:31:51 -0700

    Add files via upload

---
+++ JAVAS/Java_backend/src/server/SocketServer.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SocketServer {
+
+
+    public static  void main(String []args) {
+
+
+        try (ServerSocket server = new ServerSocket(12345))
+        {
+            System.out.println("Conecting...");
+           while (true){
+               Socket socket= server.accept();
+               System.out.println("New user connected");
+               new Client(socket).start();
+           }
+        }catch (IOException w){
+            System.out.println(w.getMessage());
+        }
+
+
+    }
+}
