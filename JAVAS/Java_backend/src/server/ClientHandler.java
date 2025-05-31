dea7a54b8d3d70a71c5a240509a0c23462553703
Mohammadamin Rashidi <bamdadrshydy@gmail.com>
2025-05-31 12:03:23 -0700

    we have clienthandler json sending and recieving now

--- JAVAS/Java_backend/src/server/ClientHandler.java
+++ JAVAS/Java_backend/src/server/ClientHandler.java
@@ -4,23 +4,33 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import API_messages.*;
+import com.google.gson.*;
 
 public class ClientHandler extends Thread {
     private Socket socket;
+    private final Gson gson = new Gson();
+
+    private Request request;
+    private Response response;
+
+
     public ClientHandler(Socket s){
         socket=s;
     }
-
     public void run(){
         try(
                 BufferedReader reader=new BufferedReader(new InputStreamReader(socket.getInputStream()));
                 PrintWriter writer =new PrintWriter(socket.getOutputStream());
         ){
-            String received;
-            while ((received=reader.readLine())!=null){
-                System.out.println("got: "+received);
-                writer.println("server got: "+received);
+            String jsonString;
+            String responseString;
 
+            while ((jsonString=reader.readLine())!=null){
+                request = gson.fromJson(jsonString, Request.class);
+                response = RequestHandler.handle(request);
+                responseString = gson.toJson(response);
+                writer.println(responseString);
             }
         }catch (IOException e){
             System.out.println(e.getMessage());
--- JAVAS/Java_backend/src/server/RequestHandler.java
+++ JAVAS/Java_backend/src/server/RequestHandler.java
@@ -1,5 +1,20 @@
 package server;
+import API_messages.Request;
+import API_messages.Response;
 import models.*;
+import com.google.gson.*;
 
 public class RequestHandler {
+
+    public static Response handle(Request req) {
+        String action = req.getAction();
+        JsonObject payload = req.getPayload();
+
+
+        switch (action) {
+
+        }
+
+        return null; //TODO: get this implemented right
+    }
 }
