8de878a62df00cabe1b77eb95c3ace5110ca17f8
Mohammadamin Rashidi <bamdadrshydy@gmail.com>
2025-06-02 11:22:49 -0700

    added little crud methods

--- JAVAS/Java_backend/src/server/DataBase.java
+++ JAVAS/Java_backend/src/server/DataBase.java
@@ -80,4 +80,84 @@ public class DataBase {
             e.printStackTrace();
         }
     }
+
+    // adding, removing and all that fancy CRUD stuff
+
+
+    public void addAccount(Account acc) {
+        ReadAndWriteLock.writeLock().lock();
+        try {
+            accounts.put(acc.getUserId(), acc);
+            saveDbFile();
+        } finally {
+            ReadAndWriteLock.writeLock().unlock();
+        }
+    }
+
+    public Account getAccount(String userId) {
+        ReadAndWriteLock.readLock().lock();
+        try {
+            return accounts.get(userId);
+        } finally {
+            ReadAndWriteLock.readLock().unlock();
+        }
+    }
+
+    public void addTrack(Track t) {
+        ReadAndWriteLock.writeLock().lock();
+        try {
+            tracks.put(t.getTrackId(), t);
+            saveDbFile();
+        }
+        finally {
+            ReadAndWriteLock.writeLock().unlock();
+        }
+    }
+
+    public Track getTrack(String id) {
+        ReadAndWriteLock.readLock().lock();
+        try {
+            return tracks.get(id);
+        }
+        finally {
+            ReadAndWriteLock.readLock().unlock();
+        }
+    }
+
+    public void addPlaylist(PlayList p) {
+        ReadAndWriteLock.writeLock().lock();
+        try {
+            playlists.put(p.getPlayListID(), p);
+            saveDbFile();
+        }
+        finally {
+            ReadAndWriteLock.writeLock().unlock();
+        }
+    }
+
+    public PlayList getPlaylist(String id) {
+        ReadAndWriteLock.readLock().lock();
+        try {
+            return playlists.get(id);
+        }
+        finally {
+            ReadAndWriteLock.readLock().unlock();
+        }
+    }
+
+
+
+    //WARNING: this is for test use only unless we want to shut the app down lol :P
+    private void clear(){
+        for(Map.Entry<String, Account> entry : accounts.entrySet()){
+            entry = null;
+        }
+        for(Map.Entry<String, Track> entry : tracks.entrySet()){
+            entry = null;
+        }
+        for(Map.Entry<String, PlayList> entry : playlists.entrySet()){
+            entry = null;
+        }
+        db = null;
+    }
 }
